5023bcd00943daf1a64d2eed0bb0a30dc160da0b
Cristiano da Silva Monteiro <cristianodasilvamonteiro@gmail.com>
2021-12-25 20:54:53 -0300

    Modificação no exercício 19

--- Exercicios/Exercicio19.js
+++ Exercicios/Exercicio19.js
@@ -2,6 +2,9 @@
 Enunciado: Um professor quer sortear um dos seus alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
 */
 
+
+// 1ª Resolução:
+/*
 var alunos = ['Aluno0', 
             'Aluno1',
             'Aluno2',
@@ -18,3 +21,21 @@ console.log(`Número sorteado: ${sorteio}`);
 
 var alunoEscolhido = sorteio;
 console.log(`O aluno escolhido foi ${alunos[alunoEscolhido]}!`);
+*/
+
+// 2ª Resolução:
+
+let alunos = ['Maria', 
+            'Ana',
+            'Marcos',
+            'Luiz',
+            'Daniel'];
+
+// função "sorteio" criada para limitar o intervalo dos números aleatórios. 
+function sorteio(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+};
+
+var alunoEscolhido = alunos[sorteio(0, alunos.length)];
+
+console.log(`O aluno escolhido foi ${alunoEscolhido}.`);
